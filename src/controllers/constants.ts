8f70972b88e66ab0587bf1dfdbb3ce5aa5d0b28c
TheUnconstrainedMind <126948085+TheUnconstrainedMind@users.noreply.github.com>
2024-04-04 18:17:57 +0400

    remove false rpcs

--- src/controllers/constants.ts
+++ src/controllers/constants.ts
@@ -15,6 +15,37 @@ const CHAINS = {
 }
 
 const RPCsByChain: RpcByChain = {
+    [CHAINS.Base]: {
+        def: [
+            "https://base.llamarpc.com",
+            "wss://base-rpc.publicnode.com",
+            "wss://base.gateway.tenderly.co",
+            "https://base.blockpi.network/v1/rpc/public"
+        ],
+        fallback: []
+    },
+
+    // TESTNET
+    [CHAINS.Mumbai]: {
+        def: [
+            "https://polygon-mumbai.g.alchemy.com/v2/demo",
+            "https://rpc.ankr.com/polygon_mumbai",
+            "https://polygon-testnet.public.blastapi.io",
+            "https://polygon-mumbai.blockpi.network/v1/rpc/public",
+            // "https://matic-testnet-archive-rpc.bwarelabs.com", // noNetwork
+            // "https://matic-mumbai.chainstacklabs.com", // noNetwork
+            "https://g.w.lavanet.xyz:443/gateway/polygon1t/rpc-http/f7ee0000000000000000000000000000",
+            "https://rpc-mumbai.maticvigil.com",
+            "https://endpoints.omniatech.io/v1/matic/mumbai/public",
+
+            'https://polygon-mumbai-bor.publicnode.com',
+            'https://polygon-mumbai-pokt.nodies.app',
+        ],
+        fallback: []
+    },
+
+
+
     [CHAINS.Polygon]: {
         def: [
             "https://polygon.llamarpc.com",
@@ -75,35 +106,7 @@ const RPCsByChain: RpcByChain = {
         fallback: [
             "https://rpc.ankr.com/optimism/74cb08b0ad4e0fc6ef41f8486f6a2b309f11519cf33e8986810642447393cdc2"
         ]
-    },
-    [CHAINS.Base]: {
-        def: [
-            "https://base.llamarpc.com",
-            "wss://base-rpc.publicnode.com",
-            "wss://base.gateway.tenderly.co",
-            "https://base.blockpi.network/v1/rpc/public"
-        ],
-        fallback: []
-    },
-
-    // TESTNET
-    [CHAINS.Mumbai]: {
-        def: [
-            "https://polygon-mumbai.g.alchemy.com/v2/demo",
-            "https://rpc.ankr.com/polygon_mumbai",
-            "https://polygon-testnet.public.blastapi.io",
-            "https://polygon-mumbai.blockpi.network/v1/rpc/public",
-            "https://matic-testnet-archive-rpc.bwarelabs.com",
-            "https://matic-mumbai.chainstacklabs.com",
-            "https://g.w.lavanet.xyz:443/gateway/polygon1t/rpc-http/f7ee0000000000000000000000000000",
-            "https://rpc-mumbai.maticvigil.com",
-            "https://endpoints.omniatech.io/v1/matic/mumbai/public",
-
-            'https://polygon-mumbai-bor.publicnode.com',
-            'https://polygon-mumbai-pokt.nodies.app',
-        ],
-        fallback: []
-    },
+    }
 }
 
 const CONTRACT_TYPES = {
